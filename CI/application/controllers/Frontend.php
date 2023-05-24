2cffba8e2e9aa6d085a422a299e41cc2cc06a19b
EasonCheng98 <eason.cheng@i-tea.com.tw>
2023-05-24 18:36:24 +0800

    test

--- CI/application/controllers/Frontend.php
+++ CI/application/controllers/Frontend.php
@@ -1,34 +1,45 @@
 <?php
     class Frontend extends CI_Controller
     {
+        private $data = [];
+
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->model("Product_model");
+        }
         
         public function home()
         {
-            $this->load->model("Product_model");
+            $this->data['arrivalList'] = $this->Product_model->get_where(array('latest'=>1, ));
+            $this->data['featuredList'] = $this->Product_model->get_where(array('featured'=>1, ));
+            $this->data['topsellList'] = $this->Product_model->get_where(array('topsell'=>1, ));
 
-            $arrivalList = $this->Product_model->get_where(array('latest'=>1, ));
-            $featuredList = $this->Product_model->get_where(array('featured'=>1, ));
-            $topsellList = $this->Product_model->get_where(array('topsell'=>1, ));
-
-            $data = [ 'arrivalList'=>$arrivalList, 'featuredList'=>$featuredList, 'topsellList'=>$topsellList,];
-
+            //$data = [ 'arrivalList'=>$arrivalList, 'featuredList'=>$featuredList, 'topsellList'=>$topsellList,];
+            //$this->data['arrivalList'] = $arrivalList;
+            //$this->data['featuredList'] = $featuredList;
+            //$this->data['topsellList'] = $topsellList;
+            
             $this->load->view("header");
-            $this->load->view("home",$data);
+            $this->load->view("home", $this->data);
             $this->load->view("footer");       
          }
 
+
         public function product_list()
         {
-            $data = [];
-            $productList =
+            
+            $this->$data['productList'] =
             [
                 ["id" => "1", "title" => "TV"],
                 ["id" => "2", "title" => "HomeCooker"],
             ];
-            $data['productList'] = $productList;
+
+            //$data['productList'] = $productList;
+            //$this->$data['productList'] = $productList ;
 
             $this->load->view("header");
-            $this->load->view("product_list", $data);
+            $this->load->view("product_list", $this->data);
             $this->load->view("footer");
 
 
@@ -36,8 +47,14 @@
 
         public function product_detail($product_id)
         {
+            
+            $this->data['productData'] = $this->Product_model->getOne(array('id'=>$product_id));
+
+            //$data = ['productData' => $productdata];
+            //$this->data['productData'] = $productdata ;
+
             $this->load->view("header");
-           
+            $this->load->view("product_detail",$this->data);
             $this->load->view("footer");
         }
     }
--- CI/application/models/Product_model.php
+++ CI/application/models/Product_model.php
@@ -6,13 +6,21 @@
             $this->load->database();
         }
 
+        public function getOne($where=array())
+        {
+            $this->db->select("*");
+            $this->db->where($where);
+            $query = $this->db->get("product");
+            return $query->row_array(); //return associatve arrary
+        }
+
         public function get_where($where=array())
         {
             //query builder
             $this->db->select("*");
             $this->db->where($where);
             $query = $this->db->get("product");
-            return $query->result_array();
+            return $query->result_array(); //return multidimensional arrary
         }
     }
 
